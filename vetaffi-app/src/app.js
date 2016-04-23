41c7cd554e6c5593693b32606c945b875a0b3071
qwwqwwq <jeffreyquinn@Jeffreys-MacBook-Air.local>
2016-04-23 16:45:52 -0700

    basic generated form

--- vetaffi-app/src/app.js
+++ vetaffi-app/src/app.js
@@ -9,18 +9,52 @@ var app = angular.module('vetaffiApp', [
     'schemaForm'
 ]);
 
-app.controller('FormController', ['formData', function ($scope, formData) {
-    $scope.schema = formData;
+app.controller('FormController', ['$scope', 'formData', function ($scope, formData) {
+    $scope.schema = {
+        "type": "object",
+        "title": "Comment",
+        "properties": {
+            "name": {
+                "title": "Name",
+                "type": "string"
+            },
+            "email": {
+                "title": "Email",
+                "type": "string",
+                "pattern": "^\\S+@\\S+$",
+                "description": "Email will be used for evil."
+            },
+            "comment": {
+                "title": "Comment",
+                "type": "string",
+                "maxLength": 20,
+                "validationMessage": "Don't be greedy!"
+            }
+        },
+        "required": [
+            "name",
+            "email",
+            "comment"
+        ]
+    };
 
     $scope.form = [
-        "*",
+        "name",
+        "email",
         {
-            type: "submit",
-            title: "Save"
+            "key": "comment",
+            "type": "textarea",
+            "placeholder": "Make a comment"
+        },
+        {
+            "type": "submit",
+            "style": "btn-info",
+            "title": "OK"
         }
     ];
 
     $scope.model = {};
+    console.log("in controller");
 }]);
 
 /**
@@ -36,7 +70,6 @@ app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when("/form", {
         templateUrl: "templates/form.tpl.html"
     });
-
     $routeProvider.otherwise({
         redirectTo: '/'
     });
