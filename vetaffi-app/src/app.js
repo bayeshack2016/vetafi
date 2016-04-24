e98cb7bf09932322daac46ef2e3ca5d9104577fc
qwwqwwq <jeffreyquinn@Jeffreys-MacBook-Air.local>
2016-04-23 18:39:19 -0700

    form progress bars

--- vetaffi-app/src/app.js
+++ vetaffi-app/src/app.js
@@ -6,7 +6,8 @@ angular.module('formData', []);
 var app = angular.module('vetaffiApp', [
     'ngRoute',
     'formData',
-    'schemaForm'
+    'schemaForm',
+    'ui.bootstrap'
 ]);
 
 app.controller('FormController', ['$scope', 'formData', function ($scope, formData) {
@@ -15,37 +16,58 @@ app.controller('FormController', ['$scope', 'formData', function ($scope, formDa
         "title": "Comment",
         "properties": {
             "name": {
+                formName: "formB",
                 "title": "Name",
                 "type": "string"
             },
             "email": {
+                formName: "formA",
                 "title": "Email",
                 "type": "string",
                 "pattern": "^\\S+@\\S+$",
                 "description": "Email will be used for evil."
             },
             "comment": {
+                formName: "formA",
                 "title": "Comment",
                 "type": "string",
                 "maxLength": 20,
-                "validationMessage": "Don't be greedy!"
+                "validationMessage": "Don't be greedy!",
+                "x-schema-form": {
+                    "type": "textarea",
+                    "placeholder": "in da schema",
+                    "condition": "model.choice == 'one'"
+                }
+
+            },
+            choice: {
+                formName: "formA",
+                type: "string",
+                enum: ["one", "two"],
+                "x-schema-form": {
+                    key: "choice",
+                    type: "radiobuttons",
+                    titleMap: [
+                        {value: "one", name: "One"},
+                        {value: "two", name: "More..."}
+                    ]
+                }
             }
         },
         "required": [
             "name",
             "email",
-            "comment"
+            "comment",
+            "something"
         ]
     };
 
     $scope.form = [
         "name",
         "email",
-        {
-            "key": "comment",
-            "type": "textarea",
-            "placeholder": "Make a comment"
-        },
+        "comment",
+        "choice",
+
         {
             "type": "submit",
             "style": "btn-info",
@@ -53,8 +75,43 @@ app.controller('FormController', ['$scope', 'formData', function ($scope, formDa
         }
     ];
 
+    $scope.vaForms = ['formA', 'formB'];
+
+    $scope.getProgress = function (formName) {
+        var total = 0;
+        var filledOut = 0;
+        for (var key in $scope.schema.properties) {
+            if ($scope.schema.properties.hasOwnProperty(key)) {
+                console.log($scope.schema.properties[key]);
+                if ($scope.schema.properties[key].formName === formName) {
+                    total += 1;
+                }
+                if ($scope.model[key]) {
+                    filledOut += 1;
+                }
+            }
+        }
+        console.log(total);
+        console.log(filledOut);
+        console.log(formName);
+        if (total == 0) {
+            return "0";
+        }
+
+        return (filledOut / total) * 100;
+    };
+
+    $scope.getType = function (formName) {
+        console.log($scope.getProgress(formName));
+        if ($scope.getProgress(formName) >= 100) {
+            return "success";
+
+        } else {
+            return "info";
+        }
+    };
+
     $scope.model = {};
-    console.log("in controller");
 }]);
 
 /**
