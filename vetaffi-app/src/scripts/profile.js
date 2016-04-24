543b0c1096d99f0ae6b58011d4863aa899b3fcd5
Aaron Hsu <aaron@nextbit.com>
2016-04-24 03:49:48 -0700

    profile

--- vetaffi-app/src/scripts/profile.js
+++ vetaffi-app/src/scripts/profile.js
@@ -1,6 +1,55 @@
 var app = angular.module('vetaffiApp');
 
-app.controller('profileCtrl', ['$scope', '$mixpanel', function($scope, $mixpanel) {
+app.controller('profileCtrl', ['$scope', '$mixpanel', '$routeParams', 'formState',
+    function($scope, $mixpanel, $routeParams) {
+
+    $scope.claimsInProgress = $routeParams.claimsInProgress;
+    $scope.pastClaims = $routeParams.pastClaims;
+    $scope.anyActions = $scope.claimsInProgress || $scope.pastClaims;
+    $scope.claimState = 'verified';
+
+    $scope.claimTextMapper = {
+        'received' : 'The VA has received your file.<br>Now verifying your information...',
+        'verified' : 'The VA has verified your information.<br>Now evaluating your claim...',
+        'evaluated': 'The VA has evaluated your claim.<br>They are finishing up soon!',
+        'finished': 'Congrats! Your health claim has been approved!'
+    };
+
+    $scope.formMapper = {
+        'ptsd' : 'VBA-21-526EZ-ARE',
+        'physical' : 'VBA-21-526EZ-ARE',
+        'general' : 'VBA-21-526EZ-ARE'
+    };
+
+    $scope.user = {
+        email: 'kittykatvet@gmail.com',
+        address: '135 Bluxome St',
+        city: 'San Francisco',
+        state: 'California',
+        zipCode: '94107',
+        country: 'United States'
+    }
+
+    $scope.stats = [
+        {
+            title: 'Name',
+            fieldName: 'name',
+            value: 'Katherine Ann Williams'
+        },
+        {
+            title: 'Rank',
+            value: 'Captain'
+        },
+        {
+            title: 'Service',
+            values: ['Marine Corps 1942-1946', 'Navy 1952-1953']
+        },
+        {
+            title: 'Discharge date',
+            value: 'Dec. 17, 2014'
+        },
+    ];
+
     $mixpanel.track("profile_page_landed", {});
 }]);
 
