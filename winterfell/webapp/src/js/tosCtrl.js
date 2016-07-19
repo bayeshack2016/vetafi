a9db03e1f0f6b842f0ea87e717ba65299f297a9c
Aaron Hsu <aaron@nextbit.com>
2016-07-18 22:05:53 -0700

    some edits to ToS page

--- winterfell/webapp/src/js/tosCtrl.js
+++ winterfell/webapp/src/js/tosCtrl.js
@@ -1,6 +1,17 @@
 var app = angular.module('vetafiApp');
 app.controller('tosCtrl', ['$scope', '$location',
   function($scope, $location) {
+    $scope.onAccept = function() {
+      var input = $('.vfi-checkbox input');
+      if (input.prop('checked')) {
+        console.log('Continue!');
+      } else {
+        $('.vfi-error-msg').text('You must check the checkbox above to agree and continue.');
+      }
+    };
 
+    $scope.onDecline = function() {
+      $location.path('/');
+    };
   }
 ]);
