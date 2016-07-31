5f895957884c026975e56b09c4882ad13c194103
qwwqwwq <jeffreyquinn@Jeffreys-MacBook-Air.local>
2016-07-31 13:20:10 -0700

    fix test script

--- winterfell/run-tests.sh
+++ winterfell/run-tests.sh
@@ -2,7 +2,7 @@
 pushd pdf-filler
 
 # First run java tests / build.
-# ./mvnw clean package
+./mvnw clean package
 
 java -Ddw.server.applicationConnectors\[0\].port=9797 -jar target/pdf-filler-1.0-SNAPSHOT.jar server config.yaml >>pdf-filler.log 2>&1  &
 pid=$!
