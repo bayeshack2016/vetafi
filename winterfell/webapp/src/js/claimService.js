a733b01b37c61995aa956ea04a7ad9a9a9faf6fb
Aaron Hsu <aaron@nextbit.com>
2016-08-03 23:46:46 -0700

    Draft for viewing specific claim

--- winterfell/webapp/src/js/claimService.js
+++ winterfell/webapp/src/js/claimService.js
@@ -6,17 +6,30 @@ app.factory('claimService', function() {
       {
         id: 'asdf',
         updatedAt: '123',
-        state: 'incomplete'
+        state: 'incomplete',
+        files: [
+          { id: 'A', name: 'FormA', type: 'va', description: 'Something about form A.' },
+          { id: 'B', name: 'FormB', type: 'va', description: 'Another thing about form B.' }
+        ]
       },
       {
         id: 'qwer',
         updatedAt: '456',
-        state: 'submitted'
+        state: 'submitted',
+        files: [
+          { id: 'A', name: 'FormA', type: 'va', description: 'Something about form A.' },
+          { id: 'C', name: 'FormC', type: 'user', description: 'Blah blah blah in form C.' }
+        ]
       },
       {
         id: 'zxcv',
         updatedAt: '999',
-        state: 'processed'
+        state: 'processed',
+        files: [
+          { id: 'A', name: 'FormA', type: 'va', description: 'Something about form A.' },
+          { id: 'B', name: 'FormB', type: 'va', description: 'Another thing about form B.' },
+          { id: 'C', name: 'FormC', type: 'user', description: 'Blah blah blah in form C.' }
+        ]
       }
     ],
     clearClaims: function() {
---
+++ winterfell/webapp/src/js/claimViewCtrl.js
@@ -0,0 +1,32 @@
+'use strict';
+var app = angular.module('vetafiApp');
+app.controller('claimViewCtrl', ['$scope', '$location', 'claimService', 'net', 'ngDialog',
+  function($scope, $location, claimService, net) {
+    $scope.claimId = $location.path().substring(7);
+    $scope.claim = {};
+
+    function updateClaim(claim) {
+      if (claim.state == 'incomplete') {
+        claim.title = 'Last modified on ' + claim.updatedAt;
+        claim.subtitle = 'This claim has not been submitted yet.';
+      } else if (claim.state == 'submitted') {
+        claim.title = 'Submitted on ' + claim.updatedAt;
+        claim.subtitle = 'This claim is still being processed by Veteran Affairs.';
+      } else if (claim.state == 'processed') {
+        claim.title = 'Processed on ' + claim.updatedAt;
+        claim.subtitle = 'The Veteran Affairs have finished processing this claim.';
+      }
+    }
+
+    //
+    // Watchers
+    //
+    $scope.$watch(function () {
+      return claimService.userClaims;
+    }, function (newVal) {
+      $scope.claim = _.find(claimService.userClaims, {id: $scope.claimId});
+      updateClaim($scope.claim);
+    });
+
+  }
+]);
