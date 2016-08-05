ef151ca0d454ec8bfe89b3c6bab403df21471999
Aaron Hsu <aaron@nextbit.com>
2016-08-05 00:48:32 -0700

    update comment

--- winterfell/webapp/src/js/claimService.js
+++ winterfell/webapp/src/js/claimService.js
@@ -8,7 +8,8 @@ app.factory('claimService', ['net',
        * Claim objects contains:
        * id
        * state
-       *
+       * updatedAt
+       * files
        */
       userClaims: [],
       clearClaims: function() {
