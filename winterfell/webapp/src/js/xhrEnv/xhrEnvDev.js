c353d46139a8a260ec0e47808b8d8d45f9ae66fe
Aaron Hsu <aaron@nextbit.com>
2016-09-08 00:46:36 -0700

    remove clientIds from xhrEnv

--- winterfell/webapp/src/js/xhrEnv/xhrEnvDev.js
+++ winterfell/webapp/src/js/xhrEnv/xhrEnvDev.js
@@ -3,8 +3,6 @@ var app = angular.module('vetafiApp');
 app.factory('xhrEnv', function() {
   return {
     isDev: true,
-    baseServerUrl: 'http://localhost:3999',
-
-    idMeClientId: '684c7204feed7758b25527eae2d66e28'
+    baseServerUrl: 'http://localhost:3999'
   };
 });
--- winterfell/webapp/src/js/xhrEnv/xhrEnvRelease.js
+++ winterfell/webapp/src/js/xhrEnv/xhrEnvRelease.js
@@ -3,8 +3,6 @@ var app = angular.module('vetafiApp');
 app.factory('xhrEnv', function() {
   return {
     isDev: false,
-    baseServerUrl: 'https://www.vetafi.org',
-
-    idMeClientId: '71ffbd3f04241a56e63fa6a960fbb15e',
+    baseServerUrl: 'https://www.vetafi.org'
   };
 });
