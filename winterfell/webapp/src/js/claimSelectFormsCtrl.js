e8140ed776f5f199d548ff987c485f036eb6259c
Aaron Hsu <aaron@nextbit.com>
2016-09-16 23:36:16 -0700

    redo select forms ui only

--- winterfell/webapp/src/js/claimSelectFormsCtrl.js
+++ winterfell/webapp/src/js/claimSelectFormsCtrl.js
@@ -1,13 +1,23 @@
 var app = angular.module('vetafiApp');
-app.controller('claimSelectFormsCtrl', ['$scope', 'claimService', 'formTemplateService', '$stateParams',
-  function($scope, claimService, formTemplateService, $stateParams) {
+app.controller('claimSelectFormsCtrl', ['$scope', 'claimService', '$stateParams',
+  function($scope, claimService, $stateParams) {
     $scope.claimId = $stateParams.claimId;
-    $scope.forms = formTemplateService;
+    $scope.allForms = [
+      {
+        title: 'VA-2392 Intent to File',
+        summary: 'This form is meant for blah blah something to hold your place in line.',
+        required: true
+      }
+    ];
+    var partitionForms = _.partition($scope.allForms, 'required');
+    $scope.requiredForms = partitionForms[0];
+    $scope.optionalForms = partitionForms[1];
+    $scope.numRequiredCompleted = 1;
+    $scope.progressBarType = $scope.numRequiredCompleted == $scope.requiredForms.length ? 'success' : undefined;
 
     $scope.onClickCancel = function() {
       console.log("Are you sure you want to cancel? You will have to start over if you do. Y/N?");
-      var claim = claimService.getIncompleteClaim();
-      claimService.removeClaim(claim.id);
+      claimService.removeClaim($stateParams.claimId);
     };
   }
 ]);
