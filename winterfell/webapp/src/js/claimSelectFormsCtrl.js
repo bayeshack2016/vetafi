1536e32d72d9246c73ad10d8e5b3a9c09bfd9f45
Aaron Hsu <aaron@nextbit.com>
2016-10-19 02:14:02 -0700

    fix ui angular elements + formly form object

--- winterfell/webapp/src/js/claimSelectFormsCtrl.js
+++ winterfell/webapp/src/js/claimSelectFormsCtrl.js
@@ -1,16 +1,18 @@
 var app = angular.module('vetafiApp');
-app.controller('claimSelectFormsCtrl', ['$scope', 'claimService', 'formTemplateService', '$stateParams', 'forms',
-  function($scope, claimService, formTemplateService, $stateParams, forms) {
+app.controller('claimSelectFormsCtrl', ['$scope', 'claimService', 'formTemplateService', '$stateParams', 'claimForms',
+  function($scope, claimService, formTemplateService, $stateParams, claimForms) {
     $scope.claimId = $stateParams.claimId;
     $scope.formTemplates = formTemplateService;
-    $scope.forms = _.keyBy(forms, function(form) {
+    $scope.forms = _.keyBy(claimForms, function(form) {
       return form.key;
     });
 
-    $scope.numRequiredCompleted = _.sum(_.map(forms, function (form) {
+    $scope.numRequiredCompleted = _.sum(_.map(claimForms, function (form) {
       return form.answered == form.answerable ? 1 : 0;
     }));
-    $scope.numRequiredForms = forms.length;
+    $scope.numRequiredForms = _.sum(_.map($scope.formTemplates, function (form) {
+      return form.vfi.required ? 1 : 0;
+    }));
 
     $scope.onClickCancel = function() {
       console.log("Are you sure you want to cancel? You will have to start over if you do. Y/N?");
@@ -18,7 +20,7 @@ app.controller('claimSelectFormsCtrl', ['$scope', 'claimService', 'formTemplateS
     };
 
     $scope.onClickDone = function() {
-      console.log("Verifying things are correct...");
+      $state.transitionTo("root.claimconfirm", {claimId: $stateParams.claimId});
     };
   }
 ]);
