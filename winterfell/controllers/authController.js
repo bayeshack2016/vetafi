bdca8815c343dd4a5b8e1ae39cac85efc93bac5b
qwwqwwq <jeffreyquinn@Jeffreys-MacBook-Air-2.local>
2016-10-31 23:37:51 -0700

    in progress

--- winterfell/controllers/authController.js
+++ winterfell/controllers/authController.js
@@ -15,7 +15,7 @@ module.exports = function (app) {
     if (req.session.key) {
       res.redirect('/');
     } else {
-      return res.render('signup', {csrf: req.csrfToken()});
+      return res.render('signup', {csrf: ''}); // todo fill in later
     }
   });
 
@@ -24,7 +24,7 @@ module.exports = function (app) {
     if (req.session.key) {
       res.redirect('/');
     } else {
-      return res.render('login', {csrf: req.csrfToken()});
+      return res.render('login', {csrf: ''}); // todo fill in later
     }
   });
 
