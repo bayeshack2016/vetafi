aef357f9c2944758c632e2f2f88dba01ff195c4f
qwwqwwq <jeffreyquinn@Jeffreys-MacBook-Air-2.local>
2016-11-17 08:04:20 -0800

    respond

--- winterfell/config/bannerConfig.js
+++ winterfell/config/bannerConfig.js
@@ -3,5 +3,5 @@
  */
 module.exports = {
   showBanner: true,
-  bannerMessage: "Hello! Vetafi.org is in beta, and not yet ready for public use. To learn more email us at info@vetafi.com"
+  bannerMessage: "Vetafi.org is in beta, and not yet ready for public use. To learn more email us at info@vetafi.com"
 };
