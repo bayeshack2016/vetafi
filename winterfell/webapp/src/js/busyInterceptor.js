543821ad3e4de8dc1788a6f0d31c7133a444cc17
qwwqwwq <jeffreyquinn@Jeffreys-MacBook-Air-2.local>
2017-01-21 14:36:03 -0800

    done

--- winterfell/webapp/src/js/busyInterceptor.js
+++ winterfell/webapp/src/js/busyInterceptor.js
@@ -1,16 +1,21 @@
 var app = angular.module('vetafiApp');
+var BUSY_DELAY = 1000; // Will not show loading graphic until 1000ms have passed and we are still waiting for responses.
 
 app.config(function ($httpProvider) {
   $httpProvider.interceptors.push('busyHttpInterceptor');
 })
-  .factory('busyHttpInterceptor', function ($q) {
+  .factory('busyHttpInterceptor', ['$q', '$timeout', function ($q, $timeout) {
     var counter = 0;
     return {
       request: function (config) {
         counter += 1;
-        if (counter !== 0) {
-          angular.element('#busy-overlay').show();
-        }
+        $timeout(
+          function () {
+            if (counter !== 0) {
+              angular.element('#busy-overlay').show();
+            }
+          },
+          BUSY_DELAY);
         return config;
       },
       response: function (response) {
@@ -35,4 +40,4 @@ app.config(function ($httpProvider) {
         return rejection;
       }
     }
-  });
+  }]);
