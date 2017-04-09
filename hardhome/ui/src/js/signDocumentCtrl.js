6120689b50ab84ef8063807f9e5033f6d280429b
Jeff Quinn <jeff@nuna.com>
2017-04-09 07:17:16 -0700

    alternative flow, not using iframe

--- hardhome/ui/src/js/signDocumentCtrl.js
+++ hardhome/ui/src/js/signDocumentCtrl.js
@@ -3,35 +3,39 @@ var app = angular.module('vetafiApp');
 app.controller('signDocumentCtrl', ['$scope', '$filter', '$stateParams', '$state', 'claimForms', 'net', '$uibModal',
     '$document', '$window',
     function ($scope, $filter, $stateParams, $state, claimForms, net, $uibModal, $document, $window) {
-      $scope.forms = claimForms;
-      $scope.currentFormIndex = 0;
-      $scope.allFormsSigned = function() {
-          var result = true;
-          for(var i = 0; i< $scope.forms.length; i++) {
-              result = result && $scope.forms[i].isSigned
-          }
-          return result;
-      };
-      $scope.showPrompt = false;
+        $scope.forms = claimForms;
+        $scope.currentFormIndex = 0;
+        $scope.allFormsSigned = testAllFormsSigned();
+        $scope.showPrompt = false;
+        $scope.claimId = $stateParams.claimId;
 
-      function handleVisibilityChange() {
-          if (!document.hidden) {
-              for(var i = 0; i< $scope.forms.length; i++) {
-                  var form = $scope.forms[i];
-                  net.getFormSignatureStatus(form.claimID, form.key).then(
-                      function success(res) {
-                          form.isSigned = res.data
-                      })
-              }
-          }
-      }
+        function testAllFormsSigned() {
+            var result = true;
+            for (var i = 0; i < $scope.forms.length; i++) {
+                result = result && $scope.forms[i].isSigned
+            }
+            return result;
+        }
 
-      $window.addEventListener("visibilitychange",
-          handleVisibilityChange,
-          false);
+        function handleVisibilityChange() {
+            if (!document.hidden) {
+                for (var i = 0; i < $scope.forms.length; i++) {
+                    var form = $scope.forms[i];
+                    net.getFormSignatureStatus(form.claimID, form.key).then(
+                        function success(res) {
+                            form.isSigned = res.data;
+                            $scope.allFormsSigned = testAllFormsSigned();
+                        })
+                }
+            }
+        }
 
-      $scope.markCurrentlySigningForm = function (form) {
-          $scope.currentlySigningForm = form
-      };
+        $window.addEventListener("visibilitychange",
+            handleVisibilityChange,
+            false);
+
+        $scope.markCurrentlySigningForm = function (form) {
+            $scope.currentlySigningForm = form
+        };
     }
 ]);
