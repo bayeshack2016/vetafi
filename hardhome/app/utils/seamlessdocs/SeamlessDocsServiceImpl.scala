8821765c99fb51cbf7822bf79ef30e4c720fe69e
Jeff Quinn <jeff@nuna.com>
2017-04-25 11:19:14 -0700

    fix some javasctipts

--- hardhome/app/utils/seamlessdocs/SeamlessDocsServiceImpl.scala
+++ hardhome/app/utils/seamlessdocs/SeamlessDocsServiceImpl.scala
@@ -21,11 +21,11 @@ class SeamlessDocsServiceImpl @Inject() (
 ) extends SeamlessDocsService {
 
   val url: String = configuration.getString("seamlessdocs.url").get
-  lazy val apiSecret: Array[Byte] = secretsManager.getSecret("seamlessdocs_secret_key")
-  lazy val apiKey: String = secretsManager.getSecretUtf8("seamlessdocs_api_key")
+  // lazy val apiSecret: Array[Byte] = secretsManager.getSecret("seamlessdocs_secret_key")
+  // lazy val apiKey: String = secretsManager.getSecretUtf8("seamlessdocs_api_key")
 
-  // lazy val apiSecret: Array[Byte] = configuration.getString("seamlessdocs.secret_key").get.getBytes
-  // lazy val apiKey: String = configuration.getString("seamlessdocs.api_key").get
+  lazy val apiSecret: Array[Byte] = configuration.getString("seamlessdocs.secret_key").get.getBytes
+  lazy val apiKey: String = configuration.getString("seamlessdocs.api_key").get
   lazy val requestUtils: RequestUtils = new RequestUtils(Clock.systemUTC())
 
   private def signRequest(request: WSRequest): WSRequest = {
