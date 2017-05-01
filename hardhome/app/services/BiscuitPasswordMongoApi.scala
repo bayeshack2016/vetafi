0159cfea95b502a89cbdec4f447a5564fce6368e
Jeff Quinn <jeff@nuna.com>
2017-04-30 23:29:12 -0700

    trying to hack reactive mongo to work right

---
+++ hardhome/app/services/BiscuitPasswordMongoApi.scala
@@ -0,0 +1,82 @@
+package services
+
+import javax.inject.Inject
+
+import play.api.inject.ApplicationLifecycle
+import play.api._
+import play.modules.reactivemongo.{DefaultReactiveMongoApi, ReactiveMongoApi}
+import reactivemongo.api.MongoConnection.ParsedURI
+import reactivemongo.api.gridfs.GridFS
+import reactivemongo.api.{DefaultDB, MongoConnection, MongoDriver}
+import reactivemongo.play.json.JSONSerializationPack
+import utils.secrets.SecretsManager
+
+import scala.concurrent.{Await, Future}
+import scala.util.{Failure, Try}
+
+/**
+  * Created by jeffquinn on 4/30/17.
+  */
+class BiscuitPasswordMongoApi @Inject() (secretsManager: SecretsManager,
+                                         configuration: Configuration,
+                                         environment: Environment,
+                                         applicationLifecycle: ApplicationLifecycle) extends ReactiveMongoApi {
+
+  lazy val driver: MongoDriver = new MongoDriver(Some(configuration.underlying))
+
+  def getProdUri: ParsedURI = {
+    val password: String = secretsManager.getSecretUtf8("prod::mongodb-password")
+    val username: String = secretsManager.getSecretUtf8("prod::mongodb-user")
+    val hosts: String = secretsManager.getSecretUtf8("prod::mongodb-hosts")
+    val db: String = configuration.getString("mongodb.db").getOrElse("test")
+
+    val uri: String = s"mongodb://$username:$password@$hosts/$db?ssl=true"
+    MongoConnection.parseURI(uri).get
+
+  }
+
+  def getNotProdUri: ParsedURI = {
+   MongoConnection.parseURI(configuration.getString("mongodb.uri").get).get
+  }
+
+  override def connection: MongoConnection = {
+
+    val con = driver.connection(parsedUri.get, strictUri = false).get
+    registerDriverShutdownHook(con, driver)
+    con
+
+    environment.mode match {
+      case Mode.Prod => getProdConnection
+
+    }
+
+
+  }
+
+  override def database: Future[DefaultDB] = ???
+
+  override def asyncGridFS: Future[GridFS[JSONSerializationPack.type]] = ???
+
+  override def db: DefaultDB = ???
+
+  override def gridFS: GridFS[JSONSerializationPack.type] = ???
+
+  private def registerDriverShutdownHook(connection: MongoConnection, mongoDriver: MongoDriver): Unit = {
+    import scala.concurrent.ExecutionContext.Implicits.global
+    import scala.concurrent.duration._
+
+    applicationLifecycle.addStopHook { () =>
+      Logger.info("ReactiveMongoApi stopping...")
+
+      Await.ready(connection.askClose()(10.seconds).map { _ =>
+        Logger.info("ReactiveMongoApi connections are stopped")
+      }.andThen {
+        case Failure(reason) =>
+          reason.printStackTrace()
+          mongoDriver.close() // Close anyway
+
+        case _ => mongoDriver.close()
+      }, 12.seconds)
+    }
+  }
+}
