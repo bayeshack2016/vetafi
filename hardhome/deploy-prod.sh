0acbf6bd33a32465b4211e62a20aaddb3c88653b
Jeff Quinn <jeff@nuna.com>
2017-05-07 11:31:45 -0700

    add secrets

--- hardhome/deploy-prod.sh
+++ hardhome/deploy-prod.sh
@@ -5,6 +5,9 @@ APPLICATION_SECRET=$1
 AUTHENTICATOR_CRYPTER_KEY=$2
 AUTHENTICATOR_COOKIE_SIGNER_KEY=$3
 
+BOXFUSE_USER=$(biscuit get --filename=../config/biscuit/secrets.yaml boxfuse-user)
+BOXFUSE_SECRET=$(biscuit get --filename=../config/biscuit/secrets.yaml boxfuse-secret)
+
 sbt clean dist
 
 boxfuse run -env=prod \
