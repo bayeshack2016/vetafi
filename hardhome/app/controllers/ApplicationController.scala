162c17ee2d2734696a5406d9dcdc8498011e055a
Jeff Quinn <jeff@nuna.com>
2017-05-18 08:35:55 -0700

    added survey

--- hardhome/app/controllers/ApplicationController.scala
+++ hardhome/app/controllers/ApplicationController.scala
@@ -5,6 +5,7 @@ import javax.inject.Inject
 import com.mohiva.play.silhouette.api.actions.SecuredErrorHandler
 import com.mohiva.play.silhouette.api.{ LogoutEvent, Silhouette }
 import com.mohiva.play.silhouette.impl.providers.SocialProviderRegistry
+import play.api.Configuration
 import play.api.i18n.{ I18nSupport, MessagesApi }
 import play.api.mvc.{ Action, AnyContent, Controller }
 import utils.auth.{ DefaultEnv, RedirectSecuredErrorHandler }
@@ -22,7 +23,8 @@ class ApplicationController @Inject() (
   val messagesApi: MessagesApi,
   silhouette: Silhouette[DefaultEnv],
   socialProviderRegistry: SocialProviderRegistry,
-  redirectSecuredErrorHandler: RedirectSecuredErrorHandler
+  redirectSecuredErrorHandler: RedirectSecuredErrorHandler,
+  configuration: Configuration
 )
   extends Controller with I18nSupport {
 
@@ -31,9 +33,14 @@ class ApplicationController @Inject() (
    *
    * @return The result to display.
    */
-  def signOut = silhouette.SecuredAction(redirectSecuredErrorHandler).async { implicit request =>
+  def signOut: Action[AnyContent] = silhouette.SecuredAction(redirectSecuredErrorHandler).async { implicit request =>
     val result = Redirect(routes.GulpAssets.index())
     silhouette.env.eventBus.publish(LogoutEvent(request.identity, request))
     silhouette.env.authenticatorService.discard(request.authenticator, result)
+  }
+
+  def googleForm: Action[AnyContent] = silhouette.UnsecuredAction {
+    implicit request =>
+      Ok(views.html.googleForm(configuration.getString("google.formUrl")))
   }
 }
